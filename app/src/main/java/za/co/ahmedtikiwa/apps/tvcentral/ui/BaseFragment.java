3fa7735389ce3076667de64bac194ed10572fccd
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2017-06-16 13:17:01 +0200

    [WIP changes] show details screen

--- app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/ui/BaseFragment.java
+++ app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/ui/BaseFragment.java
@@ -9,6 +9,9 @@ public class BaseFragment extends Fragment {
 
     public static final int COLUMN_POSTER_PATH = 1;
     public static final int COLUMN_SHOW_ID = 3;
+    public static final int COLUMN_BACKDROP_PATH = 4;
+    public static final int COLUMN_OVERVIEW_PATH = 6;
+    public static final int COLUMN_NAME_PATH = 10;
 
     public interface Callback {
         public void onItemSelected(Uri showUri);
--- app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/ui/ShowDetailFragment.java
+++ app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/ui/ShowDetailFragment.java
@@ -11,18 +11,39 @@ import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import za.co.ahmedtikiwa.apps.tvcentral.R;
+import za.co.ahmedtikiwa.apps.tvcentral.utils.Constants;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class ShowDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
+import static za.co.ahmedtikiwa.apps.tvcentral.ui.BaseFragment.COLUMN_BACKDROP_PATH;
+import static za.co.ahmedtikiwa.apps.tvcentral.ui.BaseFragment.COLUMN_POSTER_PATH;
+
+public class ShowDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Uri mUri;
     public static final int DETAIL_LOADER = 0;
+    @BindView(R.id.show_backdrop)
+    ImageView showBackdrop;
+    @BindView(R.id.backdrop_progress)
+    ProgressBar backdropProgress;
+    @BindView(R.id.poster_progress)
+    ProgressBar posterProgress;
+    @BindView(R.id.show_poster)
+    ImageView showPoster;
+    @BindView(R.id.show_name)
+    TextView showName;
+    @BindView(R.id.show_info)
+    TextView showInfo;
 
     public ShowDetailFragment() {
     }
@@ -30,9 +51,11 @@ public class ShowDetailFragment extends Fragment implements LoaderManager.Loader
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_show_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_show_detail, container, false);
 
-        ButterKnife.bind(getActivity(), rootView);
+        ButterKnife.bind(this, rootView);
+
+        getActivity().setTitle(null);
 
         Bundle bundle = getArguments();
         mUri = bundle.getParcelable(BaseFragment.SHOW_DETAIL_URI);
@@ -57,7 +80,43 @@ public class ShowDetailFragment extends Fragment implements LoaderManager.Loader
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         if (data != null && data.moveToNext()) {
-            Toast.makeText(getActivity(), data.getString(BaseFragment.COLUMN_POSTER_PATH), Toast.LENGTH_SHORT).show();
+
+            showName.setText(data.getString(BaseFragment.COLUMN_NAME_PATH));
+            showInfo.setText(data.getString(BaseFragment.COLUMN_OVERVIEW_PATH));
+
+            String backdropUrl = Constants.TMDB_IMAGE_BASE_URL + Constants.TMDB_IMAGE_BACKDROP_SIZE + data.getString(COLUMN_BACKDROP_PATH);
+            String posterUrl = Constants.TMDB_IMAGE_BASE_URL + Constants.TMDB_IMAGE_BACKDROP_SIZE + data.getString(COLUMN_POSTER_PATH);
+            Glide.with(getContext())
+                    .load(backdropUrl)
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            backdropProgress.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
+                    .into(showBackdrop);
+
+            Glide.with(getContext())
+                    .load(posterUrl)
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            posterProgress.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
+                    .into(showPoster);
         }
     }
 
