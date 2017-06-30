7fa3f4c4e2c46273c6794fa47cb2892a67f71f79
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2017-06-30 21:44:08 +0200

    [changes] added network connectivity detection on detail view

--- app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/receivers/NetworkConnectivityReceiver.java
+++ app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/receivers/NetworkConnectivityReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+// TODO: 2017/06/30 to be replaced with GCMNetworkManager
 public class NetworkConnectivityReceiver extends BroadcastReceiver {
 
     public static NetworkConnectivityListener connectivityListener;
