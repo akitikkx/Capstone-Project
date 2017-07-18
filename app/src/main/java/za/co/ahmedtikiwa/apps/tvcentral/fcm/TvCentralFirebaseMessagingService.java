1abe7a3d8ab1f92c236f0e10fdd6a8a6d6eded2f
ahmed <ahmed@double-eye.com>
2017-07-18 16:27:55 +0200

    [WIP changes] fcm

---
+++ app/src/main/java/za/co/ahmedtikiwa/apps/tvcentral/fcm/TvCentralFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package za.co.ahmedtikiwa.apps.tvcentral.fcm;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class TvCentralFirebaseMessagingService extends FirebaseInstanceIdService {
+}
